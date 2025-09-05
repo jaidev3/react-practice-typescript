8a433a6ae722da57dfb9bdca00c77a59f1ec0d7c
JaiDev <jaidevyadav720@gmail.com>
2025-09-05 12:51:52 +0530

    Add Accordion component with styling and integrate into App

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import { CounterApp } from "./components/counter-app";
-import { BooleanHook } from "./components/testUseBooleanHook";
+// import { CounterApp } from "./components/counter-app";
+// import { BooleanHook } from "./components/testUseBooleanHook";
+import Accordion from "./components/accordian/Accordian";
 function App() {
   return (
     <>
-      <CounterApp />
-      <BooleanHook />
+      {/* <CounterApp /> */}
+      {/* <BooleanHook /> */}
+      <Accordion />
     </>
   );
 }
